a3915b2bb2ab6e96ecd4f0e0b36c937df6b0936f
Riyad-Almadani <riyad.hamz1990@gmail.com>
2021-03-01 23:24:48 +0300

    dealing with capitalize function for strings

--- .vscode/convert_to_string.py
+++ .vscode/convert_to_string.py
@@ -1,9 +1,14 @@
-# Converting number to string
+# # Converting number to string
 
-# x = int(input('Enter a number: '))
+# # x = int(input('Enter a number: '))
 
-# print('The number is ' + str(x))
+# # print('The number is ' + str(x))
 
-fist_name = 'Riyad'
-last_name = 'Almadani'
-print(fist_name + ' ' + last_name)
+# # fist_name = 'Riyad'
+# last_name = 'Almadani'
+# print(fist_name + ' ' + last_name)
+
+first_name = input('What is your first name? ')
+last_name = input('What is your last name? ')
+print('hello ' + first_name .capitalize() + ' '\
+     + last_name.capitalize())
