c994cde59bc12caf02f30561fc0c7f89abe6b1ed
Riyad-Almadani <riyad.hamz1990@gmail.com>
2021-03-01 23:15:12 +0300

    String Concepts

--- .vscode/convert_to_string.py
+++ .vscode/convert_to_string.py
@@ -1,5 +1,9 @@
 # Converting number to string
 
-x = int(input('Enter a number: '))
+# x = int(input('Enter a number: '))
 
-print('The number is ' + str(x))
+# print('The number is ' + str(x))
+
+fist_name = 'Riyad'
+last_name = 'Almadani'
+print(fist_name + ' ' + last_name)
