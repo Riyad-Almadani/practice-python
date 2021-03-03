18f36c543467b890d06f1408c5f95fbdb5d4493c
Riyad-Almadani <riyad.hamz1990@gmail.com>
2021-03-03 13:44:02 +0300

    add nested if statement

---
+++ python-if/if_with_less_than.py
@@ -0,0 +1,10 @@
+# if statement with less-than operator
+
+value = '6'
+
+if value < '8':
+    print('The value is less than 8')
+elif value < '7':
+    print('The value less than 7')
+else:
+    print('The value is greater than 8')
---
+++ python-if/nested_if.py
@@ -0,0 +1,8 @@
+# Nested if code blocks
+
+first_value = True
+second_value = '6'
+
+if first_value:
+    if second_value == '6':
+        print('Got here!')
